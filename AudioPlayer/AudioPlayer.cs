bb9d61112a113036bf5417336655e121ac6be959
Yu <yu@hiya.moe>
2024-10-16 08:13:05 -0600

    Added AudioPlayer

---
+++ AudioPlayer/AudioPlayer.cs
@@ -0,0 +1,36 @@
+﻿using NAudio.Wave;
+
+namespace MonsterDuel
+{
+    public class AudioPlayer
+    {
+        private WaveOutEvent bgmPlayer;
+        private WaveOutEvent sePlayer;
+        private AudioFileReader bgmReader;
+        private AudioFileReader seReader;
+
+        public AudioPlayer()
+        {
+            bgmPlayer = new WaveOutEvent();
+            sePlayer = new WaveOutEvent();
+        }
+
+        public void PlayBGM(string bgmPath)
+        {
+            bgmReader = new AudioFileReader(bgmPath);
+            var loopWaveStream = new LoopWaveStream(bgmReader);
+            if (bgmPlayer.PlaybackState != PlaybackState.Playing)
+            {
+                bgmPlayer.Init(loopWaveStream);
+                bgmPlayer.Play();
+            }
+        }
+
+        public void PlaySE(string sePath)
+        {
+            seReader = new AudioFileReader(sePath);
+            sePlayer.Init(seReader);
+            sePlayer.Play();
+        }
+    }
+}
---
+++ AudioPlayer/LoopWaveStream.cs
@@ -0,0 +1,49 @@
+﻿using NAudio.Wave;
+
+namespace MonsterDuel
+{
+    public class LoopWaveStream : WaveStream
+    {
+        private WaveStream loopWaveStream;
+
+        public LoopWaveStream(WaveStream source)
+        {
+            loopWaveStream = source;
+        }
+        
+        public override WaveFormat WaveFormat => loopWaveStream.WaveFormat;
+
+        public override long Length => long.MaxValue;
+
+        public override long Position
+        {
+            get => loopWaveStream.Position;
+            set => loopWaveStream.Position = value;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int totalBytesRead = 0;
+
+            while (totalBytesRead < count)
+            {
+                int bytesRead = loopWaveStream.Read(buffer, offset + totalBytesRead, count - totalBytesRead);
+                if (bytesRead == 0)
+                {
+                    loopWaveStream.Position = 0;
+                }
+                totalBytesRead += bytesRead;
+            }
+            return totalBytesRead;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                loopWaveStream.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
