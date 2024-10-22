9359ff93ad1bac0d0ac69aa863d9bd0005119d54
Yu <yu@hiya.moe>
2024-10-22 14:24:02 -0600

    Added a form for testing

---
+++ Test.Designer.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace MonsterDuel
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Test
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1440, 900);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1446, 935);
+            this.MinimumSize = new System.Drawing.Size(1446, 935);
+            this.Name = "Test";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monster Duel Test";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MonsterDuel
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
