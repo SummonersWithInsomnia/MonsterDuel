429aa82b746c50f178283ae7738d7ab36d44b903
Yu <yu@hiya.moe>
2024-10-23 02:05:17 -0600

    Added more scene effects

--- SceneEffect.cs
+++ SceneEffect.cs
@@ -101,5 +101,101 @@ namespace MonsterDuel
             source.Controls.Remove(pb);
             source.Refresh();
         }
+        
+        public static async Task<PictureBox> CutInFromTop(Form source, string filepath, int duration, int step)
+        {
+            PictureBox pb = new PictureBox
+            {
+                Size = new Size(1920, 1080),
+                Location = new Point(0, -1080),
+                ImageLocation = filepath,
+                BorderStyle = BorderStyle.None
+            };
+            
+            source.Controls.Add(pb);
+            pb.BringToFront();
+            
+            int waitTime = duration / step;
+            int move = 1080 / step;
+
+            for (int i = 0; i < step; i++)
+            {
+                Point next = new Point(pb.Location.X, (pb.Location.Y + move));
+                pb.Location = next;
+                await Task.Delay(waitTime);
+            }
+
+            Point final = new Point(0, 0);
+            pb.Location = final;
+
+            return pb;
+        }
+        
+        public static async Task<PictureBox> CutInFromBottom(Form source, string filepath, int duration, int step)
+        {
+            PictureBox pb = new PictureBox
+            {
+                Size = new Size(1920, 1080),
+                Location = new Point(0, 1080),
+                ImageLocation = filepath,
+                BorderStyle = BorderStyle.None
+            };
+            
+            source.Controls.Add(pb);
+            pb.BringToFront();
+            
+            int waitTime = duration / step;
+            int move = 1080 / step;
+
+            for (int i = 0; i < step; i++)
+            {
+                Point next = new Point(pb.Location.X, (pb.Location.Y - move));
+                pb.Location = next;
+                await Task.Delay(waitTime);
+            }
+
+            Point final = new Point(0, 0);
+            pb.Location = final;
+
+            return pb;
+        }
+        
+        public static async Task CutOutFromTop(Form source, PictureBox pb, int duration, int step)
+        {
+            int waitTime = duration / step;
+            int move = 1080 / step;
+
+            for (int i = 0; i < step; i++)
+            {
+                Point next = new Point(pb.Location.X, (pb.Location.Y - move));
+                pb.Location = next;
+                await Task.Delay(waitTime);
+            }
+
+            Point final = new Point(0, -1080);
+            pb.Location = final;
+            
+            source.Controls.Remove(pb);
+            source.Refresh();
+        }
+        
+        public static async Task CutOutFromBottom(Form source, PictureBox pb, int duration, int step)
+        {
+            int waitTime = duration / step;
+            int move = 1080 / step;
+
+            for (int i = 0; i < step; i++)
+            {
+                Point next = new Point(pb.Location.X, (pb.Location.Y + move));
+                pb.Location = next;
+                await Task.Delay(waitTime);
+            }
+
+            Point final = new Point(0, 1080);
+            pb.Location = final;
+            
+            source.Controls.Remove(pb);
+            source.Refresh();
+        }
     }
 }
