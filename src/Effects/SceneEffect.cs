425e9850a4302e1b26ad704ee7644ab7eb594ed6
Yu <yu@hiya.moe>
2024-11-21 05:19:37 -0700

    Added VSMode

--- src/Effects/SceneEffect.cs
+++ src/Effects/SceneEffect.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -195,6 +196,76 @@ namespace MonsterDuel
             pb.Location = final;
             
             source.Controls.Remove(pb);
+            source.Refresh();
+        }
+        
+        public static async Task<List<PictureBox>> CuttingInLikeClosingDoor(Form source, string leftFilepath, string rightFilepath, int duration, int step)
+        {
+            PictureBox left = new PictureBox
+            {
+                Size = new Size(640, 720),
+                Location = new Point(-640, 0),
+                ImageLocation = leftFilepath,
+                BorderStyle = BorderStyle.None
+            };
+            
+            PictureBox right = new PictureBox
+            {
+                Size = new Size(640, 720),
+                Location = new Point(1280, 0),
+                ImageLocation = rightFilepath,
+                BorderStyle = BorderStyle.None
+            };
+            
+            source.Controls.Add(left);
+            source.Controls.Add(right);
+            left.BringToFront();
+            right.BringToFront();
+            
+            int waitTime = duration / step;
+            int move = 640 / step;
+            
+            for (int i = 0; i < step; i++)
+            {
+                Point leftNext = new Point((left.Location.X + move), left.Location.Y);
+                Point rightNext = new Point((right.Location.X - move), right.Location.Y);
+                left.Location = leftNext;
+                right.Location = rightNext;
+                await Task.Delay(waitTime);
+            }
+            
+            Point leftFinal = new Point(0, 0);
+            Point rightFinal = new Point(640, 0);
+            left.Location = leftFinal;
+            right.Location = rightFinal;
+            
+            return new List<PictureBox> {left, right};
+        }
+        
+        public static async Task CuttingOutLikeOpeningDoor(Form source, List<PictureBox> pbList, int duration, int step)
+        {
+            PictureBox left = pbList[0];
+            PictureBox right = pbList[1];
+            
+            int waitTime = duration / step;
+            int move = 640 / step;
+            
+            for (int i = 0; i < step; i++)
+            {
+                Point leftNext = new Point((left.Location.X - move), left.Location.Y);
+                Point rightNext = new Point((right.Location.X + move), right.Location.Y);
+                left.Location = leftNext;
+                right.Location = rightNext;
+                await Task.Delay(waitTime);
+            }
+            
+            Point leftFinal = new Point(-640, 0);
+            Point rightFinal = new Point(1280, 0);
+            left.Location = leftFinal;
+            right.Location = rightFinal;
+            
+            source.Controls.Remove(left);
+            source.Controls.Remove(right);
             source.Refresh();
         }
     }
--- src/VSMode/VSMode.cs
+++ src/VSMode/VSMode.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
 
 namespace MonsterDuel
 {
@@ -8,6 +12,10 @@ namespace MonsterDuel
     {
         private Form sourceForm;
         private AudioPlayer audioPlayer;
+        
+        private LibVLC libVLC;
+        private MediaPlayer mediaPlayer;
+        private Media mLoopBackground;
         
         public VSMode(Form source, AudioPlayer player)
         {
@@ -17,7 +25,58 @@ namespace MonsterDuel
         
         public async Task Start()
         {
-            Console.WriteLine("OK");
+            // Game Title Background Video Player
+            // Options
+            // https://wiki.videolan.org/VLC_command-line_help/
+            // - Stop hiding the mouse cursor
+            // - No audio
+            var options = new string[]
+            {
+                "--mouse-hide-timeout=2147483647",
+                "--no-audio",
+                //"--rmtosd-mouse-events",
+                //"--mouse-events"
+            };
+            libVLC = new LibVLC(options); 
+            mediaPlayer = new MediaPlayer(libVLC);
+            mediaPlayer.EnableMouseInput = false;
+            mLoopBackground = new Media(libVLC, "MonsterDuel_Data/video/vs_mode_60.mp4");
+            vvBackground.MediaPlayer = mediaPlayer;
+            
+            // Loop range
+            mediaPlayer.PositionChanged += (sender, e) =>
+            {
+                if (mediaPlayer.Position > 0.396869f)
+                {
+                    mediaPlayer.Position = 0.0f;
+                }
+            };
+            
+            audioPlayer.PlayBGM("MonsterDuel_Data/bgm/vs_mode.mp3");
+            await Task.Delay(1700);
+            
+            List<PictureBox> pbList = await SceneEffect.CuttingInLikeClosingDoor(sourceForm,
+                "MonsterDuel_Data/effect/scene/vs_left.png", 
+                "MonsterDuel_Data/effect/scene/vs_right.png", 50, 5);
+            
+            
+            // Topmost of layer
+            
+            
+            sourceForm.Controls.Add(vvBackground);
+            // Bottommost of layer
+            
+            mediaPlayer.Play(mLoopBackground);
+            
+            await Task.Delay(6000);
+            
+            await SceneEffect.CuttingOutLikeOpeningDoor(sourceForm, pbList, 50, 2);
         }
+        
+        private VideoView vvBackground = new VideoView
+        {
+            Size = new Size(1280, 720),
+            Location = new Point(0, 0)
+        };
     }
 }
